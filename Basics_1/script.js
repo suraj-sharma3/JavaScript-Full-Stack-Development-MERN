437c57e787a0bd26fc0480f5d31285a2e78d0312
suraj-sharma3 <suraj.sharma13395@gmail.com>
2024-06-13 22:49:37 +0530

    started variables and datatypes

---
+++ Basics_1/script.js
@@ -0,0 +1,11 @@
+let num1 = 13;
+let num2 = 26;
+console.log(num1 + num2)
+
+let num3 = 2.965;
+let num4 = 5.937;
+console.log(num1 * num2);
+
+let bool1 = 1;
+let bool2 = false;
+console.log(bool1 + bool2);
